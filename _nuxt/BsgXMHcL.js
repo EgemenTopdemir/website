524e7fcd54f1759c2daa240d0d433894078cd187
Sefer Mirza <sefermirza@gmail.com>
2026-01-21 23:31:01 +0300

    publish new

---
+++ _nuxt/BsgXMHcL.js
@@ -0,0 +1,1 @@
+import{_ as o,c as t,o as c,x as s}from"./dn9jgN1r.js";const r={};function n(e,a){return c(),t("blockquote",null,[s(e.$slots,"default")])}const _=Object.assign(o(r,[["render",n]]),{__name:"ProseBlockquote"});export{_ as default};
